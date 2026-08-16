cf617781303b96724e12c2e99e2335f6767cef16
Sergio Enrique Caballero Neira <scaballeron@unbosque.edu.co>
2026-08-16 15:30:17 -0500

    Update Astro config by removing Cloudflare adapter
    Removed Cloudflare adapter and output settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-  output: 'server',
+  site: 'https://sergio-enrique-caballero-neira.github.io',
   integrations: [react()]
 });
